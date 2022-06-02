5507b559bf092288e0b8b3d5a7fd24d05cd62bc2
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-02 19:28:01 +0000

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log('are we in')
+let questionString = document.getElementById('question-string')
+let optionString = document.getElementById('option-string')
+
+
+
+let questions = [ {
+    question: 'hi',
+
+alternatives: [
+    {a: 'hola', correct, true },
+    {b: 'hola', correct, true },
+]
+}
+  
+
+    
+
+
+   
+]
