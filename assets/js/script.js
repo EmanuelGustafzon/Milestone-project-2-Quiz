492f548812985a9690a58f7278331a4627935efd
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-02 19:57:13 +0000

    Add structure for the game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,39 @@
 let questionString = document.getElementById('question-string')
 let optionString = document.getElementById('option-string')
 
+let startQuiz = document.getElementById('start-quiz').addEventListener('click' , runQuiz);
 
 
-let questions = [ {
-    question: 'hi',
 
-alternatives: [
-    {a: 'hola', correct, true },
-    {b: 'hola', correct, true },
-]
-}
-  
+
+
+function runQuiz () {
 
     
 
+    let questionString = document.getElementById('question-string').value = '';
+}
 
-   
+
+
+
+
+
+
+/**
+ * Questions Arrey
+ */
+let questions = [ {
+    question: 'how do you say hello in spanish?',
+    years: [{a: 'Hola'},{b: 'Ciao'}, {c: 'Que tal'} , {d: 'Bueno'}], 
+},
+{
+    question: 'how do you say hello in Swedish?',
+    years: [{a: 'hej'},{b: 'nej'}, {c: 'dosa'} , {d: 'rem'}], 
+},
+{
+    question: 'how do you say hello in english?',
+    years: [{a: 'Hello'},{b: 'hi'}, {c: 'bye'} , {d: 'car'}], 
+},
+
 ]
