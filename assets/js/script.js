e5b9630f03f8f4aa92b0797a26e4740c27697764
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-03 09:50:40 +0000

    make the options show up when clicking the run button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,12 @@ startQuiz.addEventListener('click', runQuiz);
 
 
 function runQuiz () {
-    startQuiz.style.backgroundColor = "yellow";
-    startQuiz.innerHTML = "Stop Quiz and Show Result";
+    
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion() 
+    startQuiz.style.backgroundColor = "yellow";
+    startQuiz.innerHTML = "Stop Quiz and Show Result";
 }
 
 function setNextQuestion() {
@@ -23,6 +24,16 @@ showQuestion(shuffledQuestions[currentQuestionIndex])
 
 function showQuestion(question) {
 questionElement.innerText = question.question
+question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+button.classList.add('btn')
+if (answer.correct) {
+button.dataset.correct = answer.correct
+}
+button.addEventListener('click' , selectAnswer )
+options.appendChild(button)
+})
 }
 
 function selectAnswer () {
@@ -38,11 +49,11 @@ function selectAnswer () {
  */
 let questions = [ {
     question: 'how do you say hello in spanish?',
-    options: [{ text: 'Hola' , correct: true } , { text: 'Ciao', correct: false}, { text: 'Que tal', correct: false } , { text: 'Bueno', correct: false }], 
+    answers: [{ text: 'Hola' , correct: true } , { text: 'Ciao', correct: false}, { text: 'Que tal', correct: false } , { text: 'Bueno', correct: false }], 
 },
 {
     question: 'how do you say hello in Swedish?',
-    years: [{text: 'hej'},{text: 'nej'}, {text: 'dosa'} , {text: 'rem'}], 
+    answers: [{text: 'nej', correct: false},{text: 'hej', correct: true}, {text: 'dosa', correct: false} , {text: 'rem', correct: false}], 
 },
 
 
